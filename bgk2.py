efc7cdd5d27cbf58699671c6eaf1eb7c3af03437
Sergey Litvinov <slitvinov@gmail.com>
2024-03-18 14:35:48 -0400

    up

--- bgk2.py
+++ bgk2.py
@@ -1,17 +1,48 @@
 import numpy as np
+import random
+import matplotlib.pyplot as plt
+import matplotlib
+
+
+# equil
+def equil():
+    for j in range(ny + 2):
+        for i in range(nx + 2):
+            rl = rho[i, j]
+            ul = u[i, j] / cs2
+            vl = v[i, j] / cs2
+            uv = ul * vl
+            usq = u[i, j] * u[i, j]
+            vsq = v[i, j] * v[i, j]
+            sumsq = (usq + vsq) / cs22
+            sumsq2 = sumsq * (1.0 - cs2) / cs2
+            u2 = usq / cssq
+            v2 = vsq / cssq
+            feq[0, i, j] = w0 * (1.0 - sumsq)
+
+            feq[1, i, j] = w1 * (1.0 - sumsq + u2 + ul)
+            feq[2, i, j] = w1 * (1.0 - sumsq + v2 + vl)
+            feq[3, i, j] = w1 * (1.0 - sumsq + u2 - ul)
+            feq[4, i, j] = w1 * (1.0 - sumsq + v2 - vl)
+
+            feq[5, i, j] = w2 * (1.0 + sumsq2 + ul + vl + uv)
+            feq[6, i, j] = w2 * (1.0 + sumsq2 - ul + vl - uv)
+            feq[7, i, j] = w2 * (1.0 + sumsq2 - ul - vl + uv)
+            feq[8, i, j] = w2 * (1.0 + sumsq2 + ul - vl - uv)
+
 
 nx = 128
 ny = 64
 npop = 9
 
-nstep = 10001
+nstep = 101
 nout = 2000
 ndiag = 2000
 omega = 1.5
 iforce = True
 rho0, u0, v0 = 1, 0.1, 0
 uf = 0.1
-ibobs = True
+iobst = True
 nobst = 8
 
 #     lattice weights
@@ -33,8 +64,120 @@ fpois = 8.0 * visc * uf / float(ny) / float(ny)
 #     # of biased populations
 fpois = rho0 * fpois / 6.
 
-u = np.array((nx + 2, ny + 2))
-v = np.array((nx + 2, ny + 2))
-rho = np.array((nx + 2, ny + 2))
-feq = np.array((npop, nx + 2, ny + 2))
-f = np.array((npop, nx + 2, ny + 2))
+u = np.empty((nx + 2, ny + 2))
+v = np.empty((nx + 2, ny + 2))
+rho = np.empty((nx + 2, ny + 2))
+feq = np.empty((npop, nx + 2, ny + 2))
+f = np.empty((npop, nx + 2, ny + 2))
+
+# init hydro
+rho[...] = rho0
+u[...] = u0
+v[...] = v0
+
+equil()
+
+# init pop
+ramp = 0.01
+random.seed(1234)
+for j in range(ny + 2):
+    for i in range(nx + 2):
+        for ip in range(0, npop):
+            f[ip, i, j] = feq[ip, i, j]
+            rr = random.uniform(-1, 1)
+            f[ip, i, j] = (1. + ramp * rr) * rho0 / float(npop)
+
+iconf = 0
+plt.ioff()
+plt.axis("off")
+plt.tight_layout()
+for istep in range(nstep):
+    if istep % 10 == 0:
+        plt.imshow(np.transpose(u), matplotlib.cm.jet)
+        plt.savefig("u.%08d.png" % istep)
+    # mbc
+    for j in range(1, ny + 1):
+        f[1, 0, j] = f[1, nx, j]
+        f[5, 0, j] = f[5, nx, j]
+        f[8, 0, j] = f[8, nx, j]
+    for j in range(1, ny + 1):
+        f[3, nx + 1, j] = f[3, 1, j]
+        f[6, nx + 1, j] = f[6, 1, j]
+        f[7, nx + 1, j] = f[7, 1, j]
+    for i in range(1, nx + 1):
+        f[4, i, ny + 1] = f[2, i, ny]
+        f[8, i, ny + 1] = f[6, i + 1, ny]
+        f[7, i, ny + 1] = f[5, i - 1, ny]
+    for i in range(1, nx + 1):
+        f[2, i, 0] = f[4, i, 1]
+        f[6, i, 0] = f[8, i - 1, 1]
+        f[5, i, 0] = f[7, i + 1, 1]
+    f[8, 0, ny + 1] = f[6, 1, ny]
+    f[5, 0, 0] = f[7, 1, 1]
+    f[7, nx + 1, ny + 1] = f[5, nx, ny]
+    f[6, nx + 1, 0] = f[8, nx, 1]
+
+    # move
+    for j in range(ny, 0, -1):
+        for i in range(1, nx + 1):
+            f[2, i, j] = f[2, i, j - 1]
+            f[6, i, j] = f[6, i + 1, j - 1]
+    for j in range(ny, 0, -1):
+        for i in range(nx, 0, -1):
+            f[1, i, j] = f[1, i - 1, j]
+            f[5, i, j] = f[5, i - 1, j - 1]
+
+    for j in range(1, ny + 1):
+        for i in range(nx, 0, -1):
+            f[4, i, j] = f[4, i, j + 1]
+            f[8, i, j] = f[8, i - 1, j + 1]
+
+    for j in range(1, ny + 1):
+        for i in range(1, nx + 1):
+            f[3, i, j] = f[3, i + 1, j]
+            f[7, i, j] = f[7, i + 1, j + 1]
+
+    # hydrovar
+    for j in range(1, ny + 1):
+        for i in range(1, nx + 1):
+            rho[i,j]= f[1, i, j] + f[2, i, j] + f[3, i, j] \
+                + f[4, i, j] + f[5, i, j] + f[6, i, j] \
+                + f[7, i, j] + f[8, i, j] + f[0, i, j]
+            rhoi = 1. / rho[i, j]
+            u[i, j] =(f[1, i, j] - f[3, i, j] + f[5, i, j] - \
+                f[6, i, j] - f[7, i, j] + f[8, i, j])* rhoi
+            v[i, j] =(f[5, i, j] + f[2, i, j]+ f[6, i, j] \
+                - f[7, i, j] - f[4, i, j] - f[8, i, j]) * rhoi
+
+    equil()
+    # colli
+    for k in range(0, npop):
+        for j in range(1, ny + 1):
+            for i in range(1, nx + 1):
+                f[k, i, j] = f[k, i, j] * (1.0 - omega) + omega * feq[k, i, j]
+    if iforce:
+        frce = fpois
+        for j in range(1, ny + 1):
+            for i in range(1, nx + 1):
+                f[1, i, j] = f[1, i, j] + frce
+                f[5, i, j] = f[5, i, j] + frce
+                f[8, i, j] = f[8, i, j] + frce
+
+                f[3, i, j] = f[3, i, j] - frce
+                f[6, i, j] = f[6, i, j] - frce
+                f[7, i, j] = f[7, i, j] - frce
+    if iobst:
+        i = nx // 4
+        jbot = ny // 2 - nobst // 2
+        jtop = ny // 2 + nobst // 2 + 1
+        for j in range(ny // 2 - nobst // 2, ny // 2 + nobst // 2 + 2):
+            f[1, i, j] = f[3, i + 1, j]
+            f[5, i, j] = f[7, i + 1, j + 1]
+            f[8, i, j] = f[6, i + 1, j - 1]
+            f[3, i, j] = f[1, i - 1, j]
+            f[7, i, j] = f[5, i - 1, j]
+            f[6, i, j] = f[8, i - 1, j]
+        f[2, i, jtop] = f[4, i, jtop + 1]
+        f[6, i, jtop] = f[8, i - 1, jtop + 1]
+        f[4, i, jbot] = f[2, i, jbot - 1]
+        f[7, i, jbot] = f[5, i - 1, jbot - 1]
