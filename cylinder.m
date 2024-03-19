320df5ed20592e4d09ca6e3c8f8d305f5c2bc826
Sergey Litvinov <slitvinov@gmail.com>
2024-03-19 16:19:44 -0400

    up

--- cylinder.m
+++ cylinder.m
@@ -27,54 +27,54 @@ bbRegion = find(obst); # Boolean mask for bounce-back cells
 
 		# INITIAL CONDITION: Poiseuille profile at equilibrium
 L = ly-2; y_phys = y-1.5;
-ux = 4 * uMax / (L*L) * (y_phys.*L-y_phys.*y_phys);
-uy = zeros(lx,ly);
+u = 4 * uMax / (L*L) * (y_phys.*L-y_phys.*y_phys);
+v = zeros(lx,ly);
 rho = 1;
 for i=1:9
-  cu = 3*(cx(i)*ux+cy(i)*uy);
-  fIn(i,:,:) = rho .* t(i) .* ( 1 + cu + 1/2*(cu.*cu) - 3/2*(ux.^2+uy.^2) );
+  cu = 3*(cx(i)*u+cy(i)*v);
+  fIn(i,:,:) = rho .* t(i) .* ( 1 + cu + 1/2*(cu.*cu) - 3/2*(u.^2+v.^2) );
 end
 
 for cycle = 1:maxT
   rho = sum(fIn);
-  ux  = reshape ( (cx * reshape(fIn,9,lx*ly)), 1,lx,ly) ./rho;
-  uy  = reshape ( (cy * reshape(fIn,9,lx*ly)), 1,lx,ly) ./rho;
+  u  = reshape ( (cx * reshape(fIn,9,lx*ly)), 1,lx,ly) ./rho;
+  v  = reshape ( (cy * reshape(fIn,9,lx*ly)), 1,lx,ly) ./rho;
 
 			 # MACROSCOPIC (DIRICHLET) BOUNDARY CONDITIONS
 			 # Inlet: Poiseuille profile
   y_phys = col-1.5;
-  ux(:,in,col) = 4 * uMax / (L*L) * (y_phys.*L-y_phys.*y_phys);
-  uy(:,in,col) = 0;
-  rho(:,in,col) = 1 ./ (1-ux(:,in,col)) .* ( sum(fIn([1,3,5],in,col)) + 2*sum(fIn([4,7,8],in,col)) );
+  u(:,in,col) = 4 * uMax / (L*L) * (y_phys.*L-y_phys.*y_phys);
+  v(:,in,col) = 0;
+  rho(:,in,col) = 1 ./ (1-u(:,in,col)) .* ( sum(fIn([1,3,5],in,col)) + 2*sum(fIn([4,7,8],in,col)) );
 
 				# Outlet: Constant pressure
   rho(:,out,col) = 1;
-  ux(:,out,col) = -1 + 1 ./ (rho(:,out,col)) .* ( sum(fIn([1,3,5],out,col)) + 2*sum(fIn([2,6,9],out,col)) );
-  uy(:,out,col)  = 0;
+  u(:,out,col) = -1 + 1 ./ (rho(:,out,col)) .* ( sum(fIn([1,3,5],out,col)) + 2*sum(fIn([2,6,9],out,col)) );
+  v(:,out,col)  = 0;
 
 		  # MICROSCOPIC BOUNDARY CONDITIONS: INLET (Zou/He BC)
-  fIn(2,in,col) = fIn(4,in,col) + 2/3*rho(:,in,col).*ux(:,in,col);
+  fIn(2,in,col) = fIn(4,in,col) + 2/3*rho(:,in,col).*u(:,in,col);
   fIn(6,in,col) = fIn(8,in,col) + 1/2*(fIn(5,in,col)-fIn(3,in,col)) ...
-		  + 1/2*rho(:,in,col).*uy(:,in,col) ...
-		  + 1/6*rho(:,in,col).*ux(:,in,col);
+		  + 1/2*rho(:,in,col).*v(:,in,col) ...
+		  + 1/6*rho(:,in,col).*u(:,in,col);
   fIn(9,in,col) = fIn(7,in,col) + 1/2*(fIn(3,in,col)-fIn(5,in,col)) ...
-		  - 1/2*rho(:,in,col).*uy(:,in,col) ...
-		  + 1/6*rho(:,in,col).*ux(:,in,col);
+		  - 1/2*rho(:,in,col).*v(:,in,col) ...
+		  + 1/6*rho(:,in,col).*u(:,in,col);
 
 		 # MICROSCOPIC BOUNDARY CONDITIONS: OUTLET (Zou/He BC)
-  fIn(4,out,col) = fIn(2,out,col) - 2/3*rho(:,out,col).*ux(:,out,col);
+  fIn(4,out,col) = fIn(2,out,col) - 2/3*rho(:,out,col).*u(:,out,col);
   fIn(8,out,col) = fIn(6,out,col) + 1/2*(fIn(3,out,col)-fIn(5,out,col)) ...
-		   - 1/2*rho(:,out,col).*uy(:,out,col) ...
-		   - 1/6*rho(:,out,col).*ux(:,out,col);
+		   - 1/2*rho(:,out,col).*v(:,out,col) ...
+		   - 1/6*rho(:,out,col).*u(:,out,col);
   fIn(7,out,col) = fIn(9,out,col) + 1/2*(fIn(5,out,col)-fIn(3,out,col)) ...
-		   + 1/2*rho(:,out,col).*uy(:,out,col) ...
-		   - 1/6*rho(:,out,col).*ux(:,out,col);
+		   + 1/2*rho(:,out,col).*v(:,out,col) ...
+		   - 1/6*rho(:,out,col).*u(:,out,col);
 
 				# COLLISION STEP
   for i=1:9
-    cu = 3*(cx(i)*ux+cy(i)*uy);
+    cu = 3*(cx(i)*u+cy(i)*v);
     fEq(i,:,:)  = rho .* t(i) .* ...
-		  ( 1 + cu + 1/2*(cu.*cu)  - 3/2*(ux.^2+uy.^2) );
+		  ( 1 + cu + 1/2*(cu.*cu)  - 3/2*(u.^2+v.^2) );
     fOut(i,:,:) = fIn(i,:,:) - omega .* (fIn(i,:,:)-fEq(i,:,:));
   end
 
@@ -92,9 +92,10 @@ for cycle = 1:maxT
   if (mod(cycle,tPlot)==0)
     path = sprintf("cyl.%09d.raw", cycle);
     fid = fopen(path, "w");
-    for field = {ux, uy, rho, rho}
+    for field = {u, v, rho, rho}
       fwrite(fid, field{1}, "double");
     end
     fclose(fid);
+    disp([var(u(:)), var(v(:)), var(rho(:))]);
   end
 end
