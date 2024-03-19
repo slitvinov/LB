75d06e1bebabcabbf4033ef19702c63970f75a82
Sergey Litvinov <slitvinov@gmail.com>
2024-03-18 21:18:32 -0400

    plot vorticity

--- post2.py
+++ post2.py
@@ -4,14 +4,17 @@ import sys
 import numpy as np
 import re
 
-nx, ny = 64 * 8, 128 * 8
-plt.axis("off")
-plt.axis("equal")
-plt.tight_layout()
+nx, ny = 64 * 32, 128 * 32
 dtype = np.float64
 
 for path in sys.argv[1:]:
     u, v, rho, vort = np.memmap(path, dtype=dtype, shape=(4, nx, ny))
-    plt.imshow(u, matplotlib.cm.jet)
+    plt.axis("off")
+    plt.axis("equal")
+    plt.tight_layout()
+    lo, hi = np.quantile(vort, [0.05, 0.95])
+    plt.imshow(vort, matplotlib.cm.jet, vmin=lo, vmax=hi)
     png = re.sub("\.raw$", "", path) + ".png"
-    plt.savefig(png)
+    print(png)
+    plt.savefig(png, bbox_inches=0, dpi=500)
+    plt.close()
