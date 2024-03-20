38985ae32f122f4fe8402b937229bd2f2030696c
Sergey Litvinov <slitvinov@gmail.com>
2024-03-20 16:38:16 -0400

    working version

--- cylinder.py
+++ cylinder.py
@@ -11,7 +11,7 @@ Re = 100
 nu = uMax * 2. * obst_r / Re
 omega = 1. / (3 * nu + 1. / 2.)
 maxT = 400000
-tPlot = 50
+tPlot = 500
 t = [4 / 9, 1 / 9, 1 / 9, 1 / 9, 1 / 9, 1 / 36, 1 / 36, 1 / 36, 1 / 36]
 cx = [0, 1, 0, -1, 0, 1, -1, -1, 1]
 cy = [0, 0, 1, 0, -1, 1, 1, -1, -1]
@@ -23,17 +23,18 @@ obst[:, 0] = 1
 obst[:, ny - 1] = 1
 bb = np.where(obst)
 L = ny - 2
-y_phys = y - 1.5
+y_phys = y - 0.5
 u = 4 * uMax / (L * L) * (y_phys * L - y_phys * y_phys)
-v = np.empty((nx, ny))
-rho = np.empty((nx, ny))
+v = np.zeros((nx, ny))
 rho0 = 1
-f = np.empty((9, nx, ny))
+rho = np.full((nx, ny), rho0, dtype=np.float64)
+f = [np.empty((nx, ny)) for i in range(9)]
+f1 = [np.empty((nx, ny)) for i in range(9)]
 
 for i in range(9):
     cu = 3 * (cx[i] * u + cy[i] * v)
-    f[i, :, :] = rho0 * t[i] * (1 + cu + 1 / 2 * (cu * cu) - 3 / 2 *
-                                (u**2 + v**2))
+    f[i][:] = rho0 * t[i] * (1 + cu + 1 / 2 * (cu * cu) - 3 / 2 *
+                             (u**2 + v**2))
 
 for cycle in range(maxT):
     rho[:] = f[0] + f[1] + f[2] + f[3] + f[4] + f[5] + f[6] + f[7] + f[8]
@@ -41,7 +42,7 @@ for cycle in range(maxT):
     v[:] = (f[5] + f[2] + f[6] - f[7] - f[4] - f[8]) / rho
 
     # inlet: Poiseuille profile
-    y_phys = col - 1.5
+    y_phys = col - 0.5
     u[0, col] = 4 * uMax / (L * L) * (y_phys * L - y_phys * y_phys)
     v[0, col] = 0
     rho[0, col] = 1 / (
@@ -80,20 +81,23 @@ for cycle in range(maxT):
         feq[i][:] = rho * t[i] * (1 + cu + 1 / 2 * (cu * cu) - 3 / 2 *
                                   (u**2 + v**2))
     # Collision step
-    f1 = f * (1.0 - omega) + omega * feq
-    f1 = f - omega * (f - feq)
+    for i in range(9):
+        f1[i][:] = f[i] * (1 - omega) + omega * feq[i]
 
     # Bounce-back boundary condition
     for i in range(9):
-        f1[i, bb[0], bb[1]] = f[opp[i], bb[0], bb[1]]
+        f1[i][bb[0], bb[1]] = f[opp[i]][bb[0], bb[1]]
 
     # Streaming step
     for i in range(9):
-        f[i, :, :] = np.roll(f1[i, :, :], (cx[i], cy[i]), axis=(0, 1))
-
+        f[i][:] = np.roll(f1[i], (cx[i], cy[i]), axis=(0, 1))
     if cycle % tPlot == 0:
+        print("%.3g %.3g %.3g" % (np.var(u), np.var(v), np.var(rho)))
         path = "cyl.%09d.raw" % cycle
+        vort = np.roll(u, [0, 1], [0, 1]) - np.roll(
+            u, [0, -1], [0, 1]) - np.roll(v, [1, 0], [0, 1]) + np.roll(
+                v, [-1, 0], [0, 1])
         with open(path, "wb") as fid:
-            for field in [u, v, rho, rho]:
-                fid.write(field.tobytes())
-        print(np.var(u), np.var(v), np.var(rho))
+            for field in [u, v, rho, vort]:
+                field[bb[0], bb[1]] = np.nan
+                fid.write(field.tobytes("F"))
